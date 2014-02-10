73e76947704f1711b22eaedd3391c3e50baf2d4c
Tammie <Tammie@10.10.10.104>
2014-02-09 20:52:02 -0600

    Added properties and methods to classes. need to work on HRService

--- src/dip/lab1/student/solution1/SalariedPlusBonusEmployee.java
+++ src/dip/lab1/student/solution1/SalariedPlusBonusEmployee.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package dip.lab1.student.solution1;
 
 /**
@@ -11,5 +5,69 @@ package dip.lab1.student.solution1;
  * @author Tammie
  */
 public class SalariedPlusBonusEmployee extends HRService implements Employee{
+    private String firstName;
+    private String lastName;
+    private int employeeSSN;
+    private double yearlySalary;
+    private double biweeklySalary;
+    private static final double PAY_WEEKS = 26;
+    private double yearlyBonus;
+
+    public SalariedPlusBonusEmployee() {
+    }
+
+    @Override
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public int getEmployeeSSN() {
+        return employeeSSN;
+    }
+
+    public void setEmployeeSSN(int employeeSSN) {
+        this.employeeSSN = employeeSSN;
+    }
+
+    @Override
+    public double getYearlySalary() {
+        return yearlySalary;
+    }
+
+    public void setYearlySalary(double yearlySalary) {
+        this.yearlySalary = yearlySalary;
+    }
+
+    public double getBiweeklySalary() {
+        return biweeklySalary;
+    }
+
+    public void setBiweeklySalary(double biweeklySalary) {
+        this.biweeklySalary = biweeklySalary;
+    }
+
+    public double getYearlyBonus() {
+        return yearlyBonus;
+    }
+
+    public void setYearlyBonus(double yearlyBonus) {
+        this.yearlyBonus = yearlyBonus;
+    }
+    
+    
     
 }
