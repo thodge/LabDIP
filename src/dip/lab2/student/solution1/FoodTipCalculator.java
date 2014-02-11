a13cb8b0c082e5b382146c4c9dad327e0bb2d82f
Tammie <Tammie@Tammie-Envy>
2014-02-11 13:17:11 -0600

    worked on lab 2. got classes and constants set up

---
+++ src/dip/lab2/student/solution1/FoodTipCalculator.java
@@ -0,0 +1,16 @@
+package dip.lab2.student.solution1;
+
+/**
+ *
+ * @author Tammie
+ */
+public class FoodTipCalculator implements TipCalculatorStrategy {
+
+    private static final double MIN_BILL = 0.00;
+    private static final String BILL_ENTRY_ERR
+            = "Error: bill must be greater than or equal to " + MIN_BILL;
+    private static final double GOOD_RATE = 0.20;
+    private static final double FAIR_RATE = 0.15;
+    private static final double POOR_RATE = 0.10;
+
+}
