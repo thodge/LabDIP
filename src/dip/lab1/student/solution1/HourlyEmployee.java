0922d086e49b14d5d44b7f4fb74952e6f3873e61
Tammie <Tammie@10.30.18.217>
2014-02-11 16:35:19 -0600

    lab 1 working. pesky constructors

--- src/dip/lab1/student/solution1/HourlyEmployee.java
+++ src/dip/lab1/student/solution1/HourlyEmployee.java
@@ -4,17 +4,38 @@ package dip.lab1.student.solution1;
  *
  * @author Tammie
  */
-public class HourlyEmployee extends HRService implements Employee{
+public class HourlyEmployee implements Employee{
     private String firstName;
     private String lastName;
     private int employeeSSN;
-    private double yearlySalary;
+    private double yearlyWage;
     private double biweeklyHoursWorked;
     private double hourlyWage;
     private static final double PAY_WEEKS = 26;
 
-    public HourlyEmployee() {
+    public HourlyEmployee(double hourlyWage, double biweeklyHoursWorked) {
+        setHourlyWage(hourlyWage);
+        setBiweeklyHoursWorked(biweeklyHoursWorked);
+        
     }
+
+    public double getBiweeklyHoursWorked() {
+        return biweeklyHoursWorked;
+    }
+
+    public void setBiweeklyHoursWorked(double biweeklyHoursWorked) {
+        this.biweeklyHoursWorked = biweeklyHoursWorked;
+    }
+    
+
+    public double getHourlyWage() {
+        return hourlyWage;
+    }
+
+    public void setHourlyWage(double hourlyWage) {
+        this.hourlyWage = hourlyWage;
+    }
+    
     
 
     @Override
@@ -45,13 +66,13 @@ public class HourlyEmployee extends HRService implements Employee{
     }
 
     @Override
-    public double getYearlySalary() {
-        yearlySalary = hourlyWage * biweeklyHoursWorked * PAY_WEEKS;
-        return yearlySalary;
+    public double getYearlyWage() {
+        yearlyWage = hourlyWage * biweeklyHoursWorked;
+        return yearlyWage;
     }
 
-    public void setYearlySalary(double yearlySalary) {
-        this.yearlySalary = yearlySalary;
+    public void setYearlyWage(double yearlyWage) {
+        this.yearlyWage = yearlyWage;
     }
     
     
