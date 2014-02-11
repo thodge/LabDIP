63334ddef837b97367a154855b3adc5d42e9f78d
Tammie <Tammie@Tammie-Envy>
2014-02-11 12:43:57 -0600

    changed HRService to concrete class.  added objects to startup.  having issues with the for loop that i 'borrowed'

--- src/dip/lab1/student/solution1/Startup.java
+++ src/dip/lab1/student/solution1/Startup.java
@@ -1,9 +1,33 @@
 package dip.lab1.student.solution1;
 
+import java.text.NumberFormat;
+
 /**
  *
  * @author Tammie
  */
 public class Startup {
+    
+            //Low-level modules
+        Employee emp1 = new HourlyEmployee(10.50);
+        Employee emp2 = new SalariedEmployee(5000);
+       
+        
+        // Create a collection that we can process as a group --
+        // demands polymorphic behavior
+        Employee[] employees = {emp1, emp2};
+
+        // High-level module
+        HRService hr = new HRService();
+
+        // Just utility code to format numbers nice.
+        NumberFormat nf = NumberFormat.getCurrencyInstance();
+
+        // Test input/output by looping over collection in a way that
+        // doesn't break if we add/subtract employees from array
+        for(int i=0; i < employees.length; i++) {
+            System.out.println("Employee " + (i+1) + " compensation:" +
+            nf.format(hr.getYearlyWage(employees[i])));
+        }
     
 }
